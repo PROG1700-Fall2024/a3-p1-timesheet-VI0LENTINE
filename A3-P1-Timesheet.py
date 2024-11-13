0966648a6c515e20c6341a271827f8161be849f6
Valentine Byrnes <W0516070@campus.nscc.ca>
2024-11-13 14:57:32 -0400

    A3 P1 Time Sheet - Valentine Byrnes

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -3,18 +3,56 @@
 #               each of five work days from the user, then displays different 
 #               information calculated about those entries as output. 
 
-#Student #:     
-#Student Name:  
+#Student #: W0516070     
+#Student Name: Valentine Byrnes 
 
+
+# YOUR CODE STARTS HERE, each line must be indented (one tab)
+##########################################################################################################################################
+#Global variables
+
+days = [1, 2, 3, 4, 5]
+hrsDay = []
+
+##########################################################################################################################################
+#Define functions
+def hoursInput():
+    for i in range(len(days)):
+        hrs = int(input(f"Enter hours worked on day #{days[i]}: "))
+        hrsDay.append(hrs)
+    return hrsDay
+
+def calcHours(hrsDay):
+    mostHrs = max(hrsDay)
+    totalHrs = sum(hrsDay)
+    dayIndex = hrsDay.index(mostHrs)
+    day = days[dayIndex]
+    averageHrs = totalHrs / len(hrsDay)
+    mostHrsDays = []
+    for i in range(len(hrsDay)):
+        if hrsDay[i] == mostHrs:
+            mostHrsDays.append(days[i])
+    print("------------------------------------------------------------------------------")
+    print(f"The most hours you worked was on: ")
+    for day in mostHrsDays:
+        print(f"Day #{day} when you worked {mostHrs} hours.")
+    print("------------------------------------------------------------------------------")
+    print(f"The total number of hours you worked was: {totalHrs}")
+    print(f"The average number of hours worked each day was: {averageHrs:.1f} ")
+    print("------------------------------------------------------------------------------")
+
+def slackedOff():
+    print("Days you slacked off (i.e. worked less than 7 hours): ")
+    for i in range(len(hrsDay)):
+        slackHrs = hrsDay[i]
+        if slackHrs < 7:
+            day = days[i]
+            print(f"Day #{day}: {slackHrs} hours")
+     
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
+    hoursInput()
+    calcHours(hrsDay) 
+    slackedOff()  
 
     # YOUR CODE ENDS HERE
 
